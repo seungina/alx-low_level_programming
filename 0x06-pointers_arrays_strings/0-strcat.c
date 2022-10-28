b6f93656c0a88fee1ca33210d4e16fac181dbfbf
Regina Dennis <geenaa21@gmail.com>
2022-10-28 15:46:26 -0700

    ope

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * len - Function calculates the length of a given string.
+ * @dest: pointer to destination.
+ * Return: Always 0.
+ */
+int len(char *dest)
+{
+	return ((*dest != '\0') ? 1 + len(dest + 1) : 0);
+}
+/**
+ *_strcat - function that copies a string.
+ * @dest: This is pointer to the destination array.
+ * @src: This is the string to be appended.
+ * Return: dest.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *d_itter = dest + len(dest);
+
+	while (*src != '\0')
+{
+	*d_itter = *src;
+	src = src + 1;
+	d_itter = d_itter + 1;
+}
+*d_itter = *src;
+return (dest);
+}
