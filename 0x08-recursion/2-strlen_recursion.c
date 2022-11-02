e5fe7d8f132be92597f59a795c7af601a922f55d
regina <geenaa21@gmail.com>
2022-11-02 16:49:55 +0100

    gina

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+/**
+ * _strlen_recursion - return length of string
+ *@s: string
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+
+	return (1 + _strlen_recursion(++s));
+}
