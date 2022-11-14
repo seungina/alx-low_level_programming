04e03fc5da9ea895b7d66bbc08ca3cd6bbf82faa
regina <geenaa21@gmail.com>
2022-11-14 14:38:07 +0100

    E processor

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - the pointers are initialized.
+ * @d: typedef of structure dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == 0)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
