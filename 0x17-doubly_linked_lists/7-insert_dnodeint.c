f88ae57b5ae8ae89da79711552ff46eccef4bdf3
regina <geenaa21@gmail.com>
2023-01-12 23:41:36 +0100

    doubly

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,46 +1,41 @@
 #include "lists.h"
+
 /**
- * insert_dnodeint_at_index - inserts a new node at a given position.
- * @h: head of the linkend list
- * @idx: index to search an position
- * @n: number of index
- * Return: new node
+ * insert_dnodeint_at_index - Inserts a new node in a dlistint_t
+ *                            list at a given position.
+ * @h: A pointer to the head of the dlistint_t list.
+ * @idx: The position to insert the new node.
+ * @n: The integer for the new node to contain.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current_node;
-	dlistint_t *new_node;
+	dlistint_t *tmp = *h, *new;
 
-	if (h == NULL)
-		return (NULL);
-	if (idx > 0 && *h == NULL)
-		return (NULL);
-	new_node = malloc(sizeof(dlistint_t));
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n; /* Assing the value to new node created */
-
-	new_node->next = NULL; /* Assing NULl to declarate the of DLL*/
-	new_node->prev = NULL; /* Assing NULl to declarate the of DLL*/
-
-	if (*h == NULL) /* Validate if the second pointer is NUll  */
+	for (; idx != 1; idx--)
 	{
-		(*h) = new_node;
-		return (new_node); /* return the node end to assig pocsition  */
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
 	}
-	current_node = *h; /* Assing value of header */
 
-	for (idx = idx; idx > 1; idx--) /* scroll to the appropriate index */
-		current_node = current_node->next;
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
 
-	/* Add the next value to ->next of new node */
-	new_node->next = current_node->next;
-	/* Add to value of new node to current node */
-	current_node->next = new_node;
-	/* Assig prev node of current node */
-	new_node->prev = current_node;
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
 
-	return (new_node);
+	new->n = n;
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
+
+	return (new);
 }
